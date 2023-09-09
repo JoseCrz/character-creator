f18c900e5f0882a22d913ab7ad4f929a277f3e09
Jose Cuevas <josecueram@gmail.com>
2023-09-08 18:17:00 -0600

    Solve exercises 2, 3 & 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        id="rectangle"
+        style={{
+          position: "fixed",
+          bottom: 0,
+          height: "40%",
+          width: "100%",
+          background: "hsl(195deg, 20%, 86%)",
+        }}
+      />
       <CharacterEditor />
       <Footer />
     </>
